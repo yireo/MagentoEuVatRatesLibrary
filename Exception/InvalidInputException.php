9d5e9c2ec7f99b603095c0d4b95e62340ffa2195
Jisse Reitsma <jisse@yireo.com>
2018-12-11 09:58:00 +0100

    Fix in namespace

--- Exception/InvalidInputException.php
+++ Exception/InvalidInputException.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
 
-namespace Yireo\EuVatRates\Exception;
+namespace Yireo\MagentoEuVatRates\Exception;
 
 class InvalidInputException extends \InvalidArgumentException {}
